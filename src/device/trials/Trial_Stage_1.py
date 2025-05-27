546bcd0f1e0b61104986fea7cb14f97e59fb0f0a
Henry Burgess <henryjburg@gmail.com>
2025-05-27 16:48:36 -0500

    MBS-9 Update `Stage 1`

--- src/device/trials/Trial_Stage_1.py
+++ src/device/trials/Trial_Stage_1.py
@@ -10,6 +10,7 @@ Description: At the beginning of each trial, lit up the nose port light and deli
 
 import pygame
 from trials.Trial_Base import TrialBase
+from util import log
 
 class TrialStage1(TrialBase):
   def __init__(self, width, height):
@@ -19,16 +20,79 @@ class TrialStage1(TrialBase):
 
     # Trial state
     self.nose_port_light = False
+    self.delivered_water = False
     self.visual_cue = False
     self.nose_port_entry = False
     self.lever_press = False
     self.trial_end = False
 
+    # Trial events
+    self.events = []
+
   def on_enter(self):
     self.start_time = pygame.time.get_ticks()
 
+    # Setup trial
+    self.nose_port_light = True
+    self.display.clear_displays()
+    self.display.draw_test_pattern()
+    log("Trial started", "info")
+
+  def on_exit(self):
+    self.add_data("events", self.events)
+
   def update(self, events):
-    pass
+    # Handle any PyGame events
+    for event in events:
+      if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_ESCAPE:
+          return False
+        if event.key == pygame.K_SPACE:
+          log("Trial canceled", "info")
+          self.add_data("trial_canceled", True)
+          return False
+
+    # Handle any IO events
+    self.nose_port_entry = self.get_io().get_input_states()["nose_poke"]
+    if self.nose_port_entry:
+      self.events.append({
+        "type": "nose_port_entry",
+        "timestamp": pygame.time.get_ticks()
+      })
+      log("Nose port entry", "info")
+    self.lever_press = self.get_io().get_input_states()["left_lever"] or self.get_io().get_input_states()["right_lever"]
+    if self.get_io().get_input_states()["left_lever"] == True:
+      self.events.append({
+        "type": "left_lever_press",
+        "timestamp": pygame.time.get_ticks()
+      })
+      log("Left lever press", "info")
+    if self.get_io().get_input_states()["right_lever"] == True:
+      self.events.append({
+        "type": "right_lever_press",
+        "timestamp": pygame.time.get_ticks()
+      })
+      log("Right lever press", "info")
+
+    return True
 
   def render(self):
-    pass
+    # Deliver water if trial has started
+    if self.delivered_water == False and pygame.time.get_ticks() - self.start_time > 500:
+      self.get_io().set_water_port(True)
+    else:
+      log("Water delivered", "success")
+      self.delivered_water = True
+      self.get_io().set_water_port(False)
+
+    if self.nose_port_entry:
+      self.visual_cue = False
+      self.nose_port_light = False
+
+    # Update visual state
+    if self.visual_cue:
+      self.display.draw_test_pattern()
+    else:
+      self.display.clear_displays()
+
+    # Update nose port light
